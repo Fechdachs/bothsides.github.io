318ac51cc3c953dca32368a7e5e29a02173ea657
Frech-Dachs <166997080+Frech-Dachs@users.noreply.github.com>
2024-05-04 12:52:53 +0200

    Add files via upload

---
+++ Both-Sides/dark/dark.js
@@ -0,0 +1,27 @@
+function dark(){
+    location.href = "../dark/dark.html"
+}
+
+function about(){
+    location.href = "../about/about.html"
+}
+function credits(){
+    location.href = "../credits/credits.html"
+}
+function home(){
+    location.href = "../home/index.html"
+}
+function light(){
+    location.href = "../light/light.html"
+}
+function story(){
+    location.href = "../story/story.html"
+}
+
+function Drohne(){
+    txtDiv.style.display = "none"
+    splineDark.style.display = "none"
+    moon.style.display = "none"
+    circle.style.display = "none"
+    video.style.display = "block"
+}
